17066cfdb2c9674d9b129e197755feaea1a52680
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-05 12:23:58 +0545

    gui_label

--- GUI.label.py
+++ GUI.label.py
@@ -4,4 +4,9 @@ label1 = Label(hit,text = 'hi ')
 label2 = Label(hit,text = "My")
 label3 = Label(hit,text= "Name")
 
-label1.grid
+label1.grid(row = 0 , column = 0)
+label2.grid(row = 1 , column = 5)
+label3.grid(row = 1 , column = 1)
+
+hit.mainloop()
+
