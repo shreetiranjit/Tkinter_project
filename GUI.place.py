af991c3a6d4719cc7f9682902f9380bd8c741f29
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-05 12:20:49 +0545

    gui_place

--- GUI.place.py
+++ GUI.place.py
@@ -7,6 +7,6 @@ address = Label(geo ,text = 'Address').place(x=30 , y =90)
 contact = Label(geo , text = 'Contact').place(x=30,y=130)
 n1 = Entry(geo).place(x=80 ,y =50)
 a2 = Entry(geo).place(x=80,y=90)
-c3 = Entry(geo).place(x=80,y=130)
+c3 = Entry(geo).place(x=90,y=130)
 
 geo.mainloop()
