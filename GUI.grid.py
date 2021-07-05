c8bafd3c73fd524b0274ce3d94b8549b2383059c
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-05 12:20:06 +0545

    gui_grid

--- GUI.grid.py
+++ GUI.grid.py
@@ -1,7 +1,7 @@
 from tkinter import*
 root = Tk()
 
-name = Label(root, text='Name').grid(row= 0, column = 0)
+name = Label(root, text='NAME').grid(row= 0, column = 0)
 n1 = Entry(root).grid(row = 0 , column = 1)
 
 password = Label(root, text='Password').grid(row=1 , column = 0)
