c93b15a2490d00bfb03e513d8bf8dba6b891bbd9
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-09 11:38:11 +0545

    Entry

--- entry.py
+++ entry.py
@@ -1,13 +1,16 @@
 from tkinter import *
 root = Tk()
-e1 = Entry (root , width = 50 , fg= 'black' , bg = 'white' , borderwidth = 5 )
+e1 = Entry (root , width = 50 , fg= 'black' , bg = 'white' ,borderwidth= 15)
 e1.pack()
 def myclick() :
     textoutput = 'Hello' +  e1.get()
     myLabel = Label(root , text = textoutput)
     myLabel.pack()
+    myButton = Button(root , text = textoutput)
+    myButton.pack()
 
-myButton = Button(root , text="Click me !" , command = myclick)
+myButton = Button(root , text="Click me !" , command = myclick )
+
 myButton.pack()
 
 root.mainloop()
