f955a04b9efcef24bc33bd2a2e968941a75f6020
Shreeti <ranjit.shreeti1@gmail.com>
2021-07-12 11:44:48 +0545

    image insertion

---
+++ imageinsertion.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from PIL import ImageTk , Image
+
+root = Tk()
+root.title("image insertion")
+root.iconbitmap('C:/Users/Lenovo/OneDrive/Desktop/ne.ico')
+
+my_image = ImageTk.PhotoImage(Image.open('C:/Users/Lenovo/OneDrive/Desktop/download.png'))
+my_label = Label(image = my_image)
+my_label.pack()
+
+button_quit = Button(root , text ="EXIT" , command =root.quit,width = 20 )
+button_quit.pack()
+
+root.mainloop()
+
+
